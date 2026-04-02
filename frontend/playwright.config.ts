411ac2a865efcb7e9d940f46d61d1f7343ac1e57
Marcin Skalski <skalskimarcin33@gmail.com>
2026-04-03 00:01:57 +0200

    test(e2e): fix tests for dashboard default page
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './e2e',
-  timeout: 30_000,
+  timeout: 10_000,
   retries: process.env.CI ? 2 : 0,
   reporter: process.env.CI ? 'github' : 'list',
   use: {
