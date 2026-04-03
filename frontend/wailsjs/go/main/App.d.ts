a9ea974220e9e42fae412c637698da43709e9501
Marcin Skalski <skalskimarcin33@gmail.com>
2026-04-03 09:04:35 +0200

    fix(wailsjs): add DeleteTask binding
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- frontend/wailsjs/go/main/App.d.ts
+++ frontend/wailsjs/go/main/App.d.ts
@@ -7,6 +7,8 @@ export function AttachAgent(arg1:string):Promise<void>;
 
 export function CaptureAgentPane(arg1:string):Promise<string>;
 
+export function DeleteTask(arg1:string):Promise<void>;
+
 export function CreateTask(arg1:string,arg2:string,arg3:string):Promise<task.Task>;
 
 export function DiscoverAgents():Promise<Array<agent.Agent>>;
--- frontend/wailsjs/go/main/App.js
+++ frontend/wailsjs/go/main/App.js
@@ -10,6 +10,10 @@ export function CaptureAgentPane(arg1) {
   return window['go']['main']['App']['CaptureAgentPane'](arg1);
 }
 
+export function DeleteTask(arg1) {
+  return window['go']['main']['App']['DeleteTask'](arg1);
+}
+
 export function CreateTask(arg1, arg2, arg3) {
   return window['go']['main']['App']['CreateTask'](arg1, arg2, arg3);
 }
